017369005a249e3f02e016a1c325adff77055cc8
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-01-28 22:18:32 +0300

    1.Задание 3

---
+++ src/day1/Zadanie3.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class Zadanie3 {
+    public static void main(String[] args) {
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+
+        int anualIncomeMasha = 813120;
+        int anualIncomeDenis = 1004280;
+        int anualIncomeKristina = 914760;
+
+        int increasedSalaryMasha = salaryMasha * 10 / 100 + salaryMasha;
+        int increasedSalaryDenis = salaryDenis * 10 / 100 + salaryDenis;
+        int increasedSalaryKristina = salaryKristina * 10 / 100 + salaryKristina;
+
+        int incomeDiferenceMasha = increasedSalaryMasha * 12 - anualIncomeMasha;
+        int incomeDiferenceDenis = increasedSalaryDenis * 12 - anualIncomeDenis;
+        int incomeDiferenceKristina = increasedSalaryKristina * 12 - anualIncomeKristina;
+        System.out.println("Маша теперь получает " + increasedSalaryMasha + " рублей. Годовой доход вырос на " + incomeDiferenceMasha + " рублей.");
+        System.out.println("Денис теперь получает " + increasedSalaryDenis + " рублей. Годовой доход вырос на " + incomeDiferenceDenis + " рублей.");
+        System.out.println("Кристина теперь получает " + increasedSalaryKristina + " рублей. Годовой доход вырос на " + incomeDiferenceKristina + " рублей.");
+    }
+}
