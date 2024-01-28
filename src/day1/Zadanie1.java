2330dd29b593582a58d284f3e49c81c6066e841e
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-01-28 22:18:02 +0300

    1.Задание 1

---
+++ src/day1/Zadanie1.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        int in = 10;
+        System.out.println("Значение переменной in с типом int равно " + in);
+        byte by = 15;
+        System.out.println("Значение переменной by с типом byte равно " + by);
+        short sh = 5;
+        System.out.println("Значение переменной sh с типом short равно " + sh);
+        long lo = 100000L;
+        System.out.println("Значение переменной lo с типом long равно " + lo);
+        float fl = 12.5123F;
+        System.out.println("Значение переменной fl с типом float равно " + fl);
+        double d = 15.3;
+        System.out.println("Значение переменной d с типом double равно " + d);
+        char ch = 'B';
+        System.out.println("Значение переменной ch с типом char равно " + ch);
+        boolean bo = false;
+        System.out.println("Значение переменной bo с типом boolean равно " + bo);
+    }
+}
