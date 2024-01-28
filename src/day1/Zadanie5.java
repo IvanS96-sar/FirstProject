cd3936d861dd67516d50a286ea8f179401e711d2
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-01-28 22:20:06 +0300

    1.Задание 5

---
+++ src/day1/Zadanie5.java
@@ -0,0 +1,50 @@
+package day1;
+
+public class Zadanie5 {
+    public static void main(String[] args) {
+        int montNumber = 11;
+        switch (montNumber) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                if (montNumber > 12) {
+                    System.out.println("Номер  больше 12");
+                }
+        }
+
+    }
+}
