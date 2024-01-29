abf820d5d0f6421762b0bf575cedcbb5b182fa1f
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-01-29 20:59:59 +0300

    1.Исправил решение

--- src/day1/Zadanie4.java
+++ src/day1/Zadanie4.java
@@ -2,11 +2,11 @@ package day1;
 
 public class Zadanie4 {
     public static void main(String[] args) {
-        int temperature = 8;
+        int temperature = 4;
 
         if (temperature < 5) {
             System.out.println("На улице " + temperature + " градусов, нужно надеть шапку.");
-        } else if (temperature > 5) {
+        } else if (temperature >= 5) {
             System.out.println("На улице " + temperature + " градусов, можно идти без шапки.");
         }
     }
