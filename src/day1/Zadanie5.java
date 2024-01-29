ebcc0cf4db0fde3413431213316cf7686d842d1e
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-01-29 21:00:35 +0300

    1.Объеденил значения по сезонам

--- src/day1/Zadanie5.java
+++ src/day1/Zadanie5.java
@@ -2,43 +2,27 @@ package day1;
 
 public class Zadanie5 {
     public static void main(String[] args) {
-        int montNumber = 11;
+        int montNumber = 4;
         switch (montNumber) {
+            case 12:
             case 1:
-                System.out.println("Зима");
-                break;
             case 2:
                 System.out.println("Зима");
                 break;
             case 3:
-                System.out.println("Весна");
-                break;
             case 4:
-                System.out.println("Весна");
-                break;
             case 5:
                 System.out.println("Весна");
                 break;
             case 6:
-                System.out.println("Лето");
-                break;
             case 7:
-                System.out.println("Лето");
-                break;
             case 8:
                 System.out.println("Лето");
                 break;
             case 9:
-                System.out.println("Осень");
-                break;
             case 10:
-                System.out.println("Осень");
-                break;
             case 11:
                 System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
                 break;
             default:
                 if (montNumber > 12) {
