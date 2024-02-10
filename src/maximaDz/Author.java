0dd0d8a786a2349bba9322070feea178c3829c5e
IvanS96-sar <143840106+IvanS96-sar@users.noreply.github.com>
2024-02-10 15:54:27 +0300

    4.Initiall commit

---
+++ src/maximaDz/Author.java
@@ -0,0 +1,25 @@
+package maximaDz;
+
+public class Author {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Автор: " + name + " "
+                + surname ;
+    }
+}
